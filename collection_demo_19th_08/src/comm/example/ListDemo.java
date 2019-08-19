8976d81265b999d8d76e5e976c318baf140efbd9
ndas4945 <ndas4945@gmail.com>
2019-08-19 09:34:28 +0530

    addedd addAll and removeAll methods

--- collection_demo_19th_08/src/comm/example/ListDemo.java
+++ collection_demo_19th_08/src/comm/example/ListDemo.java
@@ -2,6 +2,8 @@ package comm.example;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ListDemo {
 
@@ -10,6 +12,12 @@ public class ListDemo {
 		Collection<String> list=new ArrayList<String>();
 		list.add("John");
 		list.add("John");
+		Set<String> set=new HashSet<String>();
+		set.add("hello");
+		set.add("world");
+		list.addAll(set);
+		System.out.println(list);
+		list.removeAll(set);
 		System.out.println(list);
 
 	}
