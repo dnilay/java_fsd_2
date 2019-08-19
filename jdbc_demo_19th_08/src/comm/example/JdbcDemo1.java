82fddc11614482988cf71ee3cc0704869bbfcff4
ndas4945 <ndas4945@gmail.com>
2019-08-19 16:42:27 +0530

    implemented singleton pattern implemented factory pattern implemented preparedStatement

--- jdbc_demo_19th_08/src/comm/example/JdbcDemo1.java
+++ jdbc_demo_19th_08/src/comm/example/JdbcDemo1.java
@@ -2,50 +2,67 @@ package comm.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class JdbcDemo1 {
-private static Properties properties=null;
-
-static {
-	properties=new Properties();
-	properties.put("JDBC_URL", "jdbc:mysql://localhost:3306/hr");
-	properties.put("USER", "root");
-	properties.put("PASSWORD", "root");
-}
+	private static Properties properties = null;
+	private static Scanner scanner=new Scanner(System.in);
+	static {
+		properties = new Properties();
+		properties.put("JDBC_URL", "jdbc:mysql://localhost:3306/hr");
+		properties.put("USER", "root");
+		properties.put("PASSWORD", "root");
+	}
 
 	public JdbcDemo1() {
-	super();
-	
-	
-}
-
-
-
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		//register the driver 
-		
-		/*
-		 * DriverManager.registerDriver(new org.h2.Driver());
-		 * Class.forName("org.h2.Driver");
-		 */
-		//Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/hr","root","root");
-		Connection connection=DriverManager.getConnection(properties.getProperty("JDBC_URL"),properties.getProperty("USER"),properties.getProperty("PASSWORD"));
-		Statement statement=connection.createStatement();
-		ResultSet resultSet=statement.executeQuery("select * from course");
-		System.out.println("ID NAME PRICE");
-		while(resultSet.next())
-		{
-			//System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getInt(3));
-			System.out.printf("\n%d %s %d", resultSet.getInt(1),resultSet.getString(2),resultSet.getInt(3));
-		}
-		
-		
-		}
+		super();
 
 	}
 
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// register the driver
+		/*
+		 * 
+		 * DriverManager.registerDriver(new org.h2.Driver());
+		 * Class.forName("org.h2.Driver");
+		 * 
+		 * //Connection
+		 * connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/hr",
+		 * "root","root"); Connection
+		 * connection=DriverManager.getConnection(properties.getProperty("JDBC_URL"),
+		 * properties.getProperty("USER"),properties.getProperty("PASSWORD")); Statement
+		 * statement=connection.createStatement(); ResultSet
+		 * resultSet=statement.executeQuery("select * from course");
+		 * System.out.println("ID NAME PRICE"); while(resultSet.next()) {
+		 * //System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "
+		 * +resultSet.getInt(3)); System.out.printf("\n%d %s %d",
+		 * resultSet.getInt(1),resultSet.getString(2),resultSet.getInt(3)); }
+		 */
+		
+		MySqlConnectionFactory factory=MySqlConnectionFactory.createConnection();
+		
+		
+		Connection connection=factory.getConnection();
+		PreparedStatement pst=connection.prepareStatement("insert into course values(?,?,?)");
+		System.out.print("ID: ");
+		int id=scanner.nextInt();
+		System.out.print("NAME: ");
+		String name=scanner.next();
+		System.out.print("AMOUNT: ");
+		int amount=scanner.nextInt();
+		pst.setInt(1, id);
+		pst.setString(2, name);
+		pst.setInt(3, amount);
+		int count=pst.executeUpdate();
+		System.out.println(count+" row(s) updated");
+		
+		
 
+	}
+
+}
---
+++ jdbc_demo_19th_08/src/comm/example/MySqlConnectionFactory.java
@@ -0,0 +1,31 @@
+package comm.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnectionFactory {
+	
+	private static Connection connection=null;
+	private static MySqlConnectionFactory connectionFactory;
+	
+	private MySqlConnectionFactory()throws SQLException
+	{
+		connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/hr","root","root");
+	}
+	
+	
+	public static MySqlConnectionFactory createConnection()throws SQLException
+	{
+		if(connectionFactory==null)
+		{
+			connectionFactory=new MySqlConnectionFactory();
+		}
+		return connectionFactory;
+	}
+	public Connection getConnection()throws SQLException
+	{
+		return connection;
+	}
+
+}
