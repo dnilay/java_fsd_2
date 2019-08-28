f3f919ea487da1e963df3f08c017c632088f8e4c
Nilay Das <mailnilaydas@gmail.com>
2019-08-28 19:32:20 +0530

    final commit

---
+++ ServletContextDemo/src/comm/example/Servlet1.java
@@ -0,0 +1,67 @@
+package comm.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doP(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doP(request,response);
+	}
+protected void doP(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	response.setContentType("text/html");
+	PrintWriter out=response.getWriter();
+	out.println(getServletContext().getInitParameter("email"));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
---
+++ ServletContextDemo/src/comm/example/listener/MyListener.java
@@ -0,0 +1,39 @@
+package comm.example.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class MyListener
+ *
+ */
+@WebListener
+public class MyListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MyListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         System.out.println("context destroyed");
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub\
+    	System.out.println("context init");
+    	
+    	//arg0.getServletContext().setAttribute(name, object);
+    	
+    }
+	
+}
