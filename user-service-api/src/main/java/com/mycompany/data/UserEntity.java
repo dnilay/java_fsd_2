28bafb813069edcaa2565e40346cc764afbbe26e
Nilay Das <mailnilaydas@gmail.com>
2019-09-16 17:50:33 +0530

    initial commit

---
+++ user-service-api/src/main/java/com/mycompany/data/UserEntity.java
@@ -0,0 +1,33 @@
+package com.mycompany.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user_table")
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "password")
+	private String password;
+	@Column(name="user_id")
+	private String userId;
+	
+
+}
