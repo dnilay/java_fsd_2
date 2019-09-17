71af6c1c4904fdd676ecd86022f04361adb5293d
Nilay Das <mailnilaydas@gmail.com>
2019-09-17 11:34:26 +0530

    initial commit-feign-client

---
+++ product-service/src/main/java/com/mycompany/Product.java
@@ -0,0 +1,21 @@
+package com.mycompany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+ private Integer id;
+
+ private String name;
+
+ private double price;
+
+
+}
